ef90d5794182c6b5b61b74e44bc0ffcd44ba09d2
FebrianAditya <febrian.aditya@student.upi.edu>
2024-06-04 22:04:16 +0700

    add code

---
+++ index.js
@@ -0,0 +1,104 @@
+// // console.log("Rachel 4 June");
+
+// // let gelasAirPutih2 = "airPutih 2"
+// // const gelasAirPutih3 = "air Putih"
+// // var gelasAirPutih4 = "air putih"
+
+
+
+
+// // // gelasAirPutih2 = "air kelapa"
+
+
+// // // let gelasAIrPutih2
+
+
+// // if (true) {
+    
+// // let firstName = "Febian"
+// // // console.log(gelasAIrPutih2s);
+// // console.log(firstName);
+// // }
+
+// // console.log(gelasAirPutih2, "==> INI YYAA");
+// // /**
+// //  * Karakterisitik
+// //  * 
+// //  * 1. reassign
+// //  * 2. redeclare
+// //  * 3. scope
+// //  * 4. hoisting
+// //  * 
+// //  * 
+// //  */
+
+// // const firstName
+
+// // if (true) {
+    
+// //     var firstName = "Rachel"
+// // }
+// // // firstName = "mahalini"
+// // console.log(firstName);
+
+
+// // var firstName = "febrian"
+
+// // firstName = "rachel"
+
+// // var firstName = "andi"
+
+// lastName = "aditya"
+
+// var lastName
+
+// // console.log(lastName);
+
+// const domba8 = "dombaaaaaaa"
+// // const 8domba
+
+// const domba7 = "dombaaaaa"
+
+
+// let sayur = 'charger hp iphone'
+
+// let statusBaterai = 28
+
+// let myFavSong = "balonku pelangi kasih ibu"
+
+
+// let myNumber = 5 + 7
+
+// let myFavSongs = ["balonku", "pelangi", "kasih ibu sepanjang masa"]
+
+// let colour = "blue"
+
+// let cekDong = colour[0]
+
+// let judulBalonku = myFavSongs[0]
+// // console.log(myFavSongs.length);
+
+
+
+let chair = {
+    isColor: "black",
+    haveArmRest: 2
+}
+
+let getColorChair = chair.isColor
+console.log(getColorChair);
+
+// console.log(chair);
+
+
+// let ageCat = 7 % 2
+// // ageCat--
+// console.log(ageCat);
+
+// // console.log(myFavSongs);
+
+
+
+
+// let thisTag = document.getElementById("firstname")
+// console.log(thisTag);
